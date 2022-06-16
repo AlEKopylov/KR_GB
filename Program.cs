79bd6779b515a40cf1e6b3fffef70d4236a9c1c6
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-06-16 22:40:30 +0300

    add clearing the array

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] inputArray = new string[5] {"Hi", "from", "Alex", "Kopylov", ":-)"};
+
+string[] ClearArray(string[] inputArr) //Create new array
+{
+    string[] resultArr = new string[inputArr.GetLength(0)];
+    int count = 0;
+    for (int i = 0; i < inputArr.GetLength(0); i++)
+    {
+        if (inputArr[i].Length <= 3)
+        {
+            resultArr[count] = inputArr[i];
+            count++;
+        }
+    }
+    return resultArr;
+}
